0094185c56db2462a19b8f9fe95190e1b91a9513
Kestrel <noah.w1406@gmail.com>
2022-11-24 01:13:09 +0100

    add miners

---
+++ bukkit/miners/src/main/java/eu/darkcube/system/miners/Miners.java
@@ -0,0 +1,12 @@
+package eu.darkcube.system.miners;
+
+import eu.darkcube.system.Plugin;
+
+public class Miners extends Plugin {
+
+	@Override
+	public String getCommandPrefix() {
+		return "";
+	}
+
+}
