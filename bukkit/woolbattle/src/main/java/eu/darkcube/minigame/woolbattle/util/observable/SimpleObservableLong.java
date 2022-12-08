bfaf9ea15005a277f6a337ea2c417ab076762988
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2022-12-08 20:50:00 +0100

    updated

--- bukkit/woolbattle/src/main/java/eu/darkcube/minigame/woolbattle/util/observable/SimpleObservableLong.java
+++
@@ -1,5 +0,0 @@
-package eu.darkcube.minigame.woolbattle.util.observable;
-
-public abstract class SimpleObservableLong extends SimpleObservableObject<Long> implements ObservableLong {
-
-}
--- bukkit/woolbattle/src/main/java/eu/darkcube/minigame/woolbattle/util/scoreboard/ScoreboardManager.java
+++
@@ -1,9 +0,0 @@
-package eu.darkcube.minigame.woolbattle.util.scoreboard;
-
-public class ScoreboardManager {
-
-	public static Scoreboard createScoreboard() {
-		return new Scoreboard();
-	}
-
-}
