2a6d4c1777bd85f08ca479dedce47701d40d6548
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2022-12-16 17:21:36 +0100

    updated

---
+++ bukkit/jumpleague/src/main/java/eu.darkcube.system.jumpleague/JumpLeague.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022. [DarkCube]
+ * All rights reserved.
+ * You may not use or redistribute this software or any associated files without permission.
+ * The above copyright notice shall be included in all copies of this software.
+ */
+
+package eu.darkcube.system.jumpleague;
+
+import eu.darkcube.system.DarkCubePlugin;
+
+public class JumpLeague extends DarkCubePlugin {
+}
