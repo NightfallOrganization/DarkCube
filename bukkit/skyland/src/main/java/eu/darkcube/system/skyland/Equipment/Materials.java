c55c24a204a11d2d6914ea1e8d9293a4d574a30c
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2022-12-31 13:40:38 +0100

    Rarity added

--- bukkit/skyland/src/main/java/eu/darkcube/system/skyland/Equipment/Materials.java
+++ bukkit/skyland/src/main/java/eu/darkcube/system/skyland/Equipment/Materials.java
@@ -2,7 +2,7 @@ package eu.darkcube.system.skyland.Equipment;
 
 public enum Materials {
 
-    DRAGON_SCALE(new PlayerStats[] {new PlayerStats(PlayerStatsType.STRENGHT, 10)})
+    DRAGON_SCALE(new PlayerStats[] {new PlayerStats(PlayerStatsType.STRENGHT, 10)}, Rarity.RARE)
 
 
 
@@ -10,8 +10,10 @@ public enum Materials {
 
 
     private PlayerStats[] stats;
-    Materials(PlayerStats[] stats){
+    private Rarity rarity;
+    Materials(PlayerStats[] stats, Rarity rarity){
         this.stats = stats;
+        this.rarity = rarity;
     }
 
 }
--- bukkit/skyland/src/main/java/eu/darkcube/system/skyland/Equipment/Rarity.java
+++ bukkit/skyland/src/main/java/eu/darkcube/system/skyland/Equipment/Rarity.java
@@ -1,5 +1,43 @@
 package eu.darkcube.system.skyland.Equipment;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public enum Rarity {
 
+    RARE("&7", 1000)
+    ;
+
+    private String prefix;
+    private int weight;
+    private static ArrayList<Rarity> rarities = new ArrayList<>();
+
+    Rarity(String prefix, int weight){
+        this.prefix = prefix;
+        this.weight = weight;
+        Rarity.getRarities().add(this);
+    }
+
+    public static ArrayList<Rarity> getRarities() {
+        return rarities;
+    }
+
+    public static Rarity rollRarity(){
+        int totalWeight = 0;
+        for (Rarity r : rarities){
+            totalWeight+= r.weight;
+        }
+
+        Random random = new Random();
+        random.nextInt(totalWeight);
+        for (Rarity r: rarities) {
+            if (totalWeight <= r.weight){
+                return r;
+            }
+            totalWeight = totalWeight-r.weight;
+        }
+
+        //sollte nie auftreten!!!
+        return null;
+    }
 }
