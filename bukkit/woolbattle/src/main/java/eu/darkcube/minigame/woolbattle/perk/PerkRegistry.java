674a304860d8918d8b14a2aef6ddf6e8437fbc70
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-03-25 19:02:00 +0100

    reenable perks

--- bukkit/woolbattle/src/main/java/eu/darkcube/minigame/woolbattle/perk/PerkRegistry.java
+++ bukkit/woolbattle/src/main/java/eu/darkcube/minigame/woolbattle/perk/PerkRegistry.java
@@ -37,7 +37,7 @@ public class PerkRegistry {
 		register(new BoosterPerk());
 		register(new GrapplingHookPerk());
 		register(new RopePerk());
-		//		register(new FreezerPerk());
+		register(new FreezerPerk());
 		register(new ExtraWoolPerk());
 		register(new RocketJumpPerk());
 		register(new LongJumpPerk());
@@ -48,12 +48,12 @@ public class PerkRegistry {
 		register(new BowPerk());
 		register(new ArrowPerk());
 		register(new ShearsPerk());
-		//		register(new ArrowBombPerk());
-		//		register(new ProtectiveShieldPerk());
+		register(new ArrowBombPerk());
+		register(new ProtectiveShieldPerk());
 		register(new DoubleJumpPerk());
-		//		register(new MinePerk());
-		//		register(new ElevatorPerk());
-		//		register(new ReflectorPerk());
+		register(new MinePerk());
+		register(new ElevatorPerk());
+		register(new ReflectorPerk());
 	}
 
 	public void register(Perk perk) {
