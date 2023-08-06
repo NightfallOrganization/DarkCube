06f664965cd81c7c3fad86e764a68bc041f66457
Darkness <voidvexum@users.noreply.github.com>
2023-08-07 00:45:53 +0200

    .

--- bukkit/citybuild/src/main/java/eu/darkcube/system/citybuild/commands/CustomMonsterSpawn.java
+++ bukkit/citybuild/src/main/java/eu/darkcube/system/citybuild/commands/CustomMonsterSpawn.java
@@ -1,68 +1,64 @@
-package eu.darkcube.system.citybuild.commands;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
-import org.bukkit.*;
-import org.bukkit.entity.*;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.CreatureSpawnEvent;
+package eu.darkcube.system.citybuild.commands;
 
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
+import org.bukkit.Location;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Enderman;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Spider;
+import org.bukkit.entity.Stray;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 
 public class CustomMonsterSpawn implements Listener {
     Random random = new Random();
+    private static final int SPAWN_CHANCE = 30;
+    private static final double CHECK_RADIUS = 15.0;
 
-    // Definieren Sie die Wahrscheinlichkeit, dass ein Monster überhaupt spawnen soll (z. B. 50%)
-    private static final int SPAWN_CHANCE = 50;
-
-    // Definieren Sie den Radius, innerhalb dessen nach vorhandenen Monstern gesucht wird
-    private static final double CHECK_RADIUS = 10.0;
+    public CustomMonsterSpawn() {
+    }// 14
 
     @EventHandler
     public void onMobSpawn(CreatureSpawnEvent event) {
-        // Überprüfen Sie, ob das Monster in der Welt "Beastrealm" gespawnt wird
-        if (event.getLocation().getWorld().getName().equals("Beastrealm")) {
-            // Überprüfen Sie, ob das Monster auf natürliche Weise gespawnt wird
-            if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.NATURAL) {
-                // Verhindern Sie das Spawnen des Monsters
-                event.setCancelled(true);
-
-                // Überprüfen Sie, ob sich bereits Monster in der Nähe befinden
-                List<Entity> nearbyEntities = event.getLocation().getWorld()
-                        .getNearbyEntities(event.getLocation(), CHECK_RADIUS, CHECK_RADIUS, CHECK_RADIUS)
-                        .stream()
-                        .filter(entity -> entity instanceof Monster)
-                        .collect(Collectors.toList());
-
-                if (nearbyEntities.isEmpty()) {
-                    // Überprüfen Sie, ob ein Monster überhaupt spawnen soll
-                    if (random.nextInt(100) < SPAWN_CHANCE) {
-                        // Generieren Sie eine Zufallszahl zwischen 1 und 100
-                        int randomNumber = random.nextInt(100) + 1;
-
-                        if (randomNumber <= 5) {
-                            spawnEntity(event.getLocation(), Creeper.class);
-                        } else if (randomNumber <= 6) {
-                            spawnEntity(event.getLocation(), Ravager.class);
-                        } else if (randomNumber <= 16) {
-                            spawnEntity(event.getLocation(), Pillager.class);
-                        } else if (randomNumber <= 21) {
-                            spawnEntity(event.getLocation(), Spider.class);
-                        } else if (randomNumber <= 23) {
-                            spawnEntity(event.getLocation(), Stray.class);
-                        } else if (randomNumber <= 70) {
-                            spawnEntity(event.getLocation(), Zombie.class);
-                        } else if (randomNumber <= 100) {
-                            spawnEntity(event.getLocation(), Skeleton.class);
-                        }
-                    }
+        if (event.getLocation().getWorld().getName().equals("Beastrealm") && event.getSpawnReason() == SpawnReason.NATURAL) {// 21 22
+            event.setCancelled(true);// 23
+            List<Entity> nearbyEntities = (List)event.getLocation().getWorld().getNearbyEntities(event.getLocation(), 15.0, 15.0, 15.0).stream().filter((entity) -> {// 25 26 27 28
+                return entity instanceof Monster;
+            }).collect(Collectors.toList());// 29
+            if (nearbyEntities.isEmpty() && this.random.nextInt(100) < 30) {// 31 32
+                int randomNumber = this.random.nextInt(100) + 1;// 33
+                if (randomNumber <= 6) {// 35
+                    this.spawnEntity(event.getLocation(), Creeper.class);// 36
+                } else if (randomNumber <= 13) {// 37
+                    this.spawnEntity(event.getLocation(), Spider.class);// 38
+                } else if (randomNumber <= 25) {// 39
+                    this.spawnEntity(event.getLocation(), Skeleton.class);// 40
+                } else if (randomNumber <= 27) {// 41
+                    this.spawnEntity(event.getLocation(), Stray.class);// 42
+                } else if (randomNumber <= 35) {// 43
+                    this.spawnEntity(event.getLocation(), Enderman.class);// 44
+                } else if (randomNumber <= 100) {// 45
+                    this.spawnEntity(event.getLocation(), Zombie.class);// 46
                 }
             }
         }
-    }
+
+    }// 52
 
     private void spawnEntity(Location location, Class<? extends LivingEntity> entityClass) {
-        location.getWorld().spawn(location, entityClass);
-    }
+        location.getWorld().spawn(location, entityClass);// 55
+    }// 56
 }
