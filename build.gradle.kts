5ed2fd8b09627bef2c0c8960cf07a2506e248d41
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-08-15 16:08:09 +0200

    build update for java 17 and CN4 Doesn't compile now because of updated CloudNet

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ allprojects {
     }
     pluginManager.withPlugin("java") {
         val extension: JavaPluginExtension = extensions.getByType(JavaPluginExtension::class)
-        extension.toolchain.languageVersion = JavaLanguageVersion.of(8)
+        extension.toolchain.languageVersion = JavaLanguageVersion.of(17)
     }
 
     tasks.withType<JavaCompile>().configureEach {
