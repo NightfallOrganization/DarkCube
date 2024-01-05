00e25fb4a3d1274289a404de9a4506b573d2d75a
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2024-01-05 13:56:58 +0100

    scoreboard no flickeridoo

--- bukkit/sumo/src/main/java/eu/darkcube/system/sumo/ruler/MainRuler.java
+++ bukkit/sumo/src/main/java/eu/darkcube/system/sumo/ruler/MainRuler.java
@@ -7,10 +7,12 @@
 
 package eu.darkcube.system.sumo.ruler;
 
+import eu.darkcube.system.sumo.other.LobbyScoreboard;
 import org.bukkit.World;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.Random;
 import java.util.List;
 import java.util.Arrays;
@@ -18,16 +20,19 @@ import java.util.Arrays;
 public class MainRuler implements Listener {
     private World activeWorld;
     private JavaPlugin plugin;
+    private LobbyScoreboard lobbyScoreboard;
     private static final List<String> AVAILABLE_MAPS = Arrays.asList("Origin", "Atlas", "Demonic");
 
-    public MainRuler(JavaPlugin plugin) {
+    public MainRuler(JavaPlugin plugin, LobbyScoreboard lobbyScoreboard) {
         this.plugin = plugin;
+        this.lobbyScoreboard = lobbyScoreboard;
         String worldName = AVAILABLE_MAPS.get(new Random().nextInt(AVAILABLE_MAPS.size()));
         setActiveWorld(Bukkit.getWorld(worldName));
     }
 
     public void setActiveWorld(World world) {
         this.activeWorld = world;
+        this.lobbyScoreboard.updateMap(world.getName());
     }
 
     public World getActiveWorld() {
