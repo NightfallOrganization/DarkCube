49765f448fd9733776b4d36d1f2962353f12a7f5
Darkness <darkness@darkcube.eu>
2024-01-21 19:50:49 +0100

    Update

--- bukkit/aetheria/src/main/java/eu/darkcube/system/aetheria/manager/monster/MonsterSpawnManager.java
+++ bukkit/aetheria/src/main/java/eu/darkcube/system/aetheria/manager/monster/MonsterSpawnManager.java
@@ -7,6 +7,7 @@
 
 package eu.darkcube.system.aetheria.manager.monster;
 
+import eu.darkcube.system.aetheria.manager.RarityManager;
 import eu.darkcube.system.aetheria.manager.WorldManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -28,16 +29,31 @@ public class MonsterSpawnManager implements Listener {
         monsterWorld = WorldManager.MONSTERWORLD;
     }
 
+    private RarityManager.Rarity getRarityForType(EntityType entityType) {
+        switch (entityType) {
+//            case ZOMBIE:
+//            case ZOMBIE_VILLAGER:
+//            case PILLAGER:
+//            case STRAY:
+//            case SKELETON:
+//            case ENDERMAN:
+//            case SPIDER:
+//                return RarityManager.Rarity.RARE;
+            case CREEPER:
+                return RarityManager.Rarity.DIVINE;
+            default:
+                return RarityManager.Rarity.ORDINARY;
+        }
+    }
+
     @EventHandler
     public void onCreatureSpawn(CreatureSpawnEvent event) {
         if (event.getLocation().getWorld().equals(monsterWorld)) {
             EntityType type = event.getEntityType();
-            if (type.equals(EntityType.ZOMBIE) || type.equals(EntityType.ZOMBIE_VILLAGER) ||
-                    type.equals(EntityType.PILLAGER) || type.equals(EntityType.STRAY) ||
-                    type.equals(EntityType.SKELETON) || type.equals(EntityType.CREEPER) ||
-                    type.equals(EntityType.ENDERMAN) || type.equals(EntityType.SPIDER)) {
-                int level = calculateMonsterLevel(event.getLocation());
-                if (level > 0) {
+            if (isMonsterType(type)) {
+                RarityManager.Rarity rarity = getRarityForType(type);
+                Integer level = calculateMonsterLevel(event.getLocation(), rarity);
+                if (level != null) {
                     monsterLevelManager.updateMonsterLevel((LivingEntity) event.getEntity(), level);
                 } else {
                     event.setCancelled(true);
@@ -48,15 +64,46 @@ public class MonsterSpawnManager implements Listener {
         }
     }
 
-    private int calculateMonsterLevel(Location location) {
-        // Berechnung des Abstands vom Punkt (0,0) in der Welt
+    private boolean isMonsterType(EntityType type) {
+        return type.equals(EntityType.ZOMBIE) || type.equals(EntityType.ZOMBIE_VILLAGER) ||
+                type.equals(EntityType.PILLAGER) || type.equals(EntityType.STRAY) ||
+                type.equals(EntityType.SKELETON) || type.equals(EntityType.CREEPER) ||
+                type.equals(EntityType.ENDERMAN) || type.equals(EntityType.SPIDER);
+    }
+
+    private Integer calculateMonsterLevel(Location location, RarityManager.Rarity rarity) {
         double distance = Math.sqrt(Math.pow(location.getX(), 2) + Math.pow(location.getZ(), 2));
+        int level = (int) Math.ceil(distance / 200.0);
 
-        // Bestimmung des Levels basierend auf dem Abstand
-        int level = (int) Math.ceil(distance / 150.0);
+        level = adjustLevelBasedOnRarity(level, rarity);
 
-        // Sicher stellen dass das Level mindestens 1 ist
-        return Math.max(level, 1);
+        return level > 0 ? level : null;
+    }
+
+    private int adjustLevelBasedOnRarity(int level, RarityManager.Rarity rarity) {
+        switch (rarity) {
+            case ORDINARY:
+                break;
+            case RARE:
+                level += 1;
+                break;
+            case EPIC:
+                level += 2;
+                break;
+            case MYTHIC:
+                level += 3;
+                break;
+            case LEGENDARY:
+                level += 4;
+                break;
+            case DIVINE:
+                level += 5;
+                break;
+            default:
+                // Keine Anpassung für ORDINARY und andere nicht spezifizierte Rarities
+                break;
+        }
+        return level;
     }
 
 }
