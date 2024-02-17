6aa1283bba24c7bc7fd8ad5523959c090043b996
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2024-02-17 19:19:06 +0100

    update

--- darkcubesystem/bukkit/src/main/java/eu/darkcube/system/bukkit/commandapi/ISuggestionProvider.java
+++ darkcubesystem/bukkit/src/main/java/eu/darkcube/system/bukkit/commandapi/ISuggestionProvider.java
@@ -7,24 +7,28 @@
 
 package eu.darkcube.system.bukkit.commandapi;
 
-import eu.darkcube.system.libs.com.mojang.brigadier.suggestion.Suggestions;
-import eu.darkcube.system.libs.com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import eu.darkcube.system.libs.com.mojang.brigadier.suggestion.Suggestions;
+import eu.darkcube.system.libs.com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public interface ISuggestionProvider {
 
     static CompletableFuture<Suggestions> suggestVec3(String input, Collection<Coordinates> coordinates, SuggestionsBuilder builder, Predicate<String> filter) {
         List<String> list = new ArrayList<>();
         if (input == null || input.isEmpty()) {
-            for (Coordinates coords : coordinates) {
-                String s = coords.x + " " + coords.y + " " + coords.z;
+            for (var coords : coordinates) {
+                var s = coords.x + " " + coords.y + " " + coords.z;
                 if (filter.test(s)) {
                     list.add(coords.x);
                     list.add(coords.x + " " + coords.y);
@@ -32,18 +36,18 @@ public interface ISuggestionProvider {
                 }
             }
         } else {
-            String[] args = input.split(" ");
+            var args = input.split(" ");
             if (args.length == 1) {
-                for (Coordinates coords : coordinates) {
-                    String s1 = args[0] + " " + coords.y + " " + coords.z;
+                for (var coords : coordinates) {
+                    var s1 = args[0] + " " + coords.y + " " + coords.z;
                     if (filter.test(s1)) {
                         list.add(args[0] + " " + coords.y);
                         list.add(s1);
                     }
                 }
             } else if (args.length == 2) {
-                for (Coordinates coords : coordinates) {
-                    String s2 = args[0] + " " + args[1] + " " + coords.z;
+                for (var coords : coordinates) {
+                    var s2 = args[0] + " " + args[1] + " " + coords.z;
                     if (filter.test(s2)) {
                         list.add(s2);
                     }
@@ -57,18 +61,18 @@ public interface ISuggestionProvider {
     static CompletableFuture<Suggestions> suggestVec2(String input, Collection<Coordinates> coordinates, SuggestionsBuilder builder, Predicate<String> filter) {
         List<String> list = new ArrayList<>();
         if (input == null || input.isEmpty()) {
-            for (Coordinates coords : coordinates) {
-                String s = coords.x + " " + coords.z;
+            for (var coords : coordinates) {
+                var s = coords.x + " " + coords.z;
                 if (filter.test(s)) {
                     list.add(coords.x);
                     list.add(s);
                 }
             }
         } else {
-            String[] args = input.split(" ");
+            var args = input.split(" ");
             if (args.length == 1) {
-                for (Coordinates coords : coordinates) {
-                    String s1 = args[0] + " " + coords.z;
+                for (var coords : coordinates) {
+                    var s1 = args[0] + " " + coords.z;
                     if (filter.test(s1)) {
                         list.add(s1);
                     }
@@ -80,8 +84,8 @@ public interface ISuggestionProvider {
     }
 
     static CompletableFuture<Suggestions> suggest(Iterable<String> suggestions, SuggestionsBuilder builder) {
-        String prefix = builder.getRemaining().toLowerCase(Locale.ROOT);
-        for (String suggestion : suggestions) {
+        var prefix = builder.getRemaining().toLowerCase(Locale.ROOT);
+        for (var suggestion : suggestions) {
             if (test(prefix, suggestion.toLowerCase(Locale.ROOT))) {
                 builder.suggest(suggestion);
             }
@@ -90,15 +94,15 @@ public interface ISuggestionProvider {
     }
 
     static CompletableFuture<Suggestions> suggest(Stream<String> suggestions, SuggestionsBuilder builder) {
-        String input = builder.getRemaining().toLowerCase(Locale.ROOT);
+        var input = builder.getRemaining().toLowerCase(Locale.ROOT);
         suggestions.filter((suggestion) -> test(input, suggestion.toLowerCase(Locale.ROOT))).forEach(builder::suggest);
         return builder.buildFuture();
     }
 
     static CompletableFuture<Suggestions> suggest(Iterable<String> suggestions, SuggestionsBuilder builder, String prefix) {
-        String input = builder.getRemaining().toLowerCase(Locale.ROOT);
-        for (String suggestion : suggestions) {
-            String suggest = prefix + suggestion;
+        var input = builder.getRemaining().toLowerCase(Locale.ROOT);
+        for (var suggestion : suggestions) {
+            var suggest = prefix + suggestion;
             if (test(input, suggest.toLowerCase(Locale.ROOT))) {
                 builder.suggest(suggest);
             }
@@ -107,9 +111,9 @@ public interface ISuggestionProvider {
     }
 
     static CompletableFuture<Suggestions> suggest(String[] suggestions, SuggestionsBuilder builder) {
-        String prefix = builder.getRemaining().toLowerCase(Locale.ROOT);
+        var prefix = builder.getRemaining().toLowerCase(Locale.ROOT);
 
-        for (String suggestion : suggestions) {
+        for (var suggestion : suggestions) {
             if (test(prefix, suggestion.toLowerCase(Locale.ROOT))) {
                 builder.suggest(suggestion);
             }
@@ -119,7 +123,7 @@ public interface ISuggestionProvider {
     }
 
     static boolean test(String input, String suggestion) {
-        for (int i = 0; !suggestion.startsWith(input, i); ++i) {
+        for (var i = 0; !suggestion.startsWith(input, i); ++i) {
             i = suggestion.indexOf('_', i);
             if (i < 0) {
                 return false;
--- darkcubesystem/bukkit/src/main/java/eu/darkcube/system/bukkit/commandapi/argument/EntityArgument.java
+++ darkcubesystem/bukkit/src/main/java/eu/darkcube/system/bukkit/commandapi/argument/EntityArgument.java
@@ -7,10 +7,13 @@
 
 package eu.darkcube.system.bukkit.commandapi.argument;
 
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+
 import com.google.common.collect.Iterables;
 import eu.darkcube.system.bukkit.commandapi.CommandSource;
 import eu.darkcube.system.bukkit.commandapi.ISuggestionProvider;
-import eu.darkcube.system.commandapi.v3.Messages;
+import eu.darkcube.system.commandapi.util.Messages;
 import eu.darkcube.system.libs.com.mojang.brigadier.StringReader;
 import eu.darkcube.system.libs.com.mojang.brigadier.arguments.ArgumentType;
 import eu.darkcube.system.libs.com.mojang.brigadier.context.CommandContext;
@@ -21,19 +24,16 @@ import eu.darkcube.system.libs.com.mojang.brigadier.suggestion.SuggestionsBuilde
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
-import java.util.Collection;
-import java.util.concurrent.CompletableFuture;
-
 public class EntityArgument implements ArgumentType<EntitySelector> {
 
-    //	public static final SimpleCommandExceptionType SELECTOR_NOT_ALLOWED = Message.SELECTOR_NOT_ALLOWED.newSimpleCommandExceptionType();
     public static final SimpleCommandExceptionType TOO_MANY_ENTITIES = Messages.TOO_MANY_ENTITIES.newSimpleCommandExceptionType();
     public static final SimpleCommandExceptionType TOO_MANY_PLAYERS = Messages.TOO_MANY_PLAYERS.newSimpleCommandExceptionType();
     public static final SimpleCommandExceptionType ONLY_PLAYERS_ALLOWED = Messages.ONLY_PLAYERS_ALLOWED.newSimpleCommandExceptionType();
     public static final SimpleCommandExceptionType ENTITY_NOT_FOUND = Messages.ENTITY_NOT_FOUND.newSimpleCommandExceptionType();
     public static final SimpleCommandExceptionType PLAYER_NOT_FOUND = Messages.PLAYER_NOT_FOUND.newSimpleCommandExceptionType();
 
-    private final boolean single, playersOnly;
+    private final boolean single;
+    private final boolean playersOnly;
 
     public EntityArgument(boolean singleIn, boolean playersOnlyIn) {
         this.single = singleIn;
@@ -45,7 +45,7 @@ public class EntityArgument implements ArgumentType<EntitySelector> {
     }
 
     public static Collection<? extends Entity> getEntities(CommandContext<CommandSource> context, String name) throws CommandSyntaxException {
-        Collection<? extends Entity> collection = EntityArgument.getEntitiesAllowingNone(context, name);
+        var collection = EntityArgument.getEntitiesAllowingNone(context, name);
         if (collection.isEmpty()) {
             throw EntityArgument.ENTITY_NOT_FOUND.create();
         }
@@ -57,7 +57,7 @@ public class EntityArgument implements ArgumentType<EntitySelector> {
     }
 
     public static Collection<Player> getPlayers(CommandContext<CommandSource> context, String name) throws CommandSyntaxException {
-        Collection<Player> list = EntityArgument.getPlayersAllowingNone(context, name);
+        var list = EntityArgument.getPlayersAllowingNone(context, name);
         if (list.isEmpty()) {
             throw EntityArgument.PLAYER_NOT_FOUND.create();
         }
@@ -89,8 +89,8 @@ public class EntityArgument implements ArgumentType<EntitySelector> {
     }
 
     @Override public EntitySelector parse(StringReader reader) throws CommandSyntaxException {
-        EntitySelectorParser entityselectorparser = new EntitySelectorParser(reader);
-        EntitySelector entityselector = entityselectorparser.parse();
+        var entityselectorparser = new EntitySelectorParser(reader);
+        var entityselector = entityselectorparser.parse();
         if (entityselector.getLimit() > 1 && this.single) {
             if (this.playersOnly) {
                 reader.setCursor(0);
@@ -108,9 +108,9 @@ public class EntityArgument implements ArgumentType<EntitySelector> {
 
     @Override public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
         if (context.getSource() instanceof ISuggestionProvider suggestionProvider) {
-            StringReader reader = new StringReader(builder.getInput());
+            var reader = new StringReader(builder.getInput());
             reader.setCursor(builder.getStart());
-            EntitySelectorParser parser = new EntitySelectorParser(reader, true);
+            var parser = new EntitySelectorParser(reader, true);
 
             try {
                 parser.parse();
@@ -118,8 +118,8 @@ public class EntityArgument implements ArgumentType<EntitySelector> {
             }
 
             return parser.fillSuggestions(builder, sbuilder -> {
-                Collection<String> collection = suggestionProvider.getPlayerNames();
-                Iterable<String> iterable = this.playersOnly ? collection : Iterables.concat(collection, suggestionProvider.getTargetedEntity());
+                var collection = suggestionProvider.getPlayerNames();
+                var iterable = this.playersOnly ? collection : Iterables.concat(collection, suggestionProvider.getTargetedEntity());
                 ISuggestionProvider.suggest(iterable, sbuilder);
             });
         }
