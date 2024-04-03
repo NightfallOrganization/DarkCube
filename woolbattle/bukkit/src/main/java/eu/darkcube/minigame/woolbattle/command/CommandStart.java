0cbdaf57bd471f1e992cc877a9c6d7752e585949
VoidVexum <114314083+VoidVexum@users.noreply.github.com>
2024-04-03 21:59:48 +0200

    Add /start to WoolBattle (#56)
    Add start command
    
    ---------
    
    Co-authored-by: Darkness <darkness@darkcube.eu>

---
+++ woolbattle/bukkit/src/main/java/eu/darkcube/minigame/woolbattle/command/CommandStart.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024. [DarkCube]
+ * All rights reserved.
+ * You may not use or redistribute this software or any associated files without permission.
+ * The above copyright notice shall be included in all copies of this software.
+ */
+
+package eu.darkcube.minigame.woolbattle.command;
+
+import eu.darkcube.minigame.woolbattle.WoolBattleBukkit;
+import eu.darkcube.minigame.woolbattle.translation.Message;
+
+public class CommandStart extends WBCommandExecutor {
+
+    public CommandStart(WoolBattleBukkit woolbattle) {
+        super("start", b -> b.executes(ctx -> {
+            woolbattle.lobby().setOverrideTimer(0);
+            ctx.getSource().sendMessage(Message.TIMER_CHANGED, 0);
+            return 0;
+        }));
+    }
+}
