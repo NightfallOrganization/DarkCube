dd0f8dfccbb087d0f332d74f80cbeae655b270c8
Darkness <darkness@darkcube.eu>
2024-04-03 21:55:37 +0200

    Update

--- woolbattle/bukkit/src/main/java/eu/darkcube/minigame/woolbattle/command/CommandStart.java
+++ woolbattle/bukkit/src/main/java/eu/darkcube/minigame/woolbattle/command/CommandStart.java
@@ -8,12 +8,16 @@
 package eu.darkcube.minigame.woolbattle.command;
 
 import eu.darkcube.minigame.woolbattle.WoolBattleBukkit;
+import eu.darkcube.minigame.woolbattle.translation.Message;
+import eu.darkcube.system.libs.com.mojang.brigadier.arguments.IntegerArgumentType;
 
 public class CommandStart extends WBCommandExecutor {
 
     public CommandStart(WoolBattleBukkit woolbattle) {
         super("start", b -> b.executes(ctx -> {
+            int time = IntegerArgumentType.getInteger(ctx, "time");
             woolbattle.lobby().setOverrideTimer(0);
+            ctx.getSource().sendMessage(Message.TIMER_CHANGED, time);
             return 0;
         }));
     }
