b3ae3aaace4031d7693099fe5239a3b210c93463
Darkness <darkness@darkcube.eu>
2024-04-05 18:37:41 +0200

    Update

--- bukkit/lobbysystem/src/main/java/eu/darkcube/system/lobbysystem/listener/ListenerRoundWalk.java
+++ bukkit/lobbysystem/src/main/java/eu/darkcube/system/lobbysystem/listener/ListenerRoundWalk.java
@@ -8,104 +8,46 @@
 package eu.darkcube.system.lobbysystem.listener;
 
 import eu.darkcube.system.lobbysystem.Lobby;
-import org.bukkit.Bukkit;
+import eu.darkcube.system.userapi.User;
+import eu.darkcube.system.userapi.UserAPI;
+import eu.darkcube.system.util.data.Key;
+import eu.darkcube.system.util.data.PersistentDataTypes;
 import org.bukkit.Location;
-import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.scheduler.BukkitRunnable;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 public class ListenerRoundWalk extends BaseListener {
-
-    private final Set<Location> requiredLocations;
-    private final Map<Player, Set<Location>> playerLocationsVisited = new HashMap<>();
-    private final Map<Player, Location> playerStartLocation = new HashMap<>();
-    private final Map<Player, Integer> playerRounds = new HashMap<>();
-    private final Map<Player, BukkitRunnable> playerResetTasks = new HashMap<>();
     private final Lobby plugin;
+    private final Key KEY_ROUND_COUNT;
 
     public ListenerRoundWalk(Lobby plugin) {
-        super();
         this.plugin = plugin;
-
-        requiredLocations = new HashSet<>();
-        requiredLocations.add(new Location(Bukkit.getWorlds().get(0), 0.5, 0, -8.5));
-        requiredLocations.add(new Location(Bukkit.getWorlds().get(0), 9.5, 0, 0.5));
-        requiredLocations.add(new Location(Bukkit.getWorlds().get(0), 0.5, 0, 9.5));
-        requiredLocations.add(new Location(Bukkit.getWorlds().get(0), -8.5, 0, 0.5));
+        this.KEY_ROUND_COUNT = new Key(plugin, "round_count");
     }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
+        User user = UserAPI.instance().user(event.getPlayer().getUniqueId());
         Player player = event.getPlayer();
+        Location spawn = plugin.getDataManager().getSpawn();
+        Location playerLocation = player.getLocation();
 
-        for (Location location : requiredLocations) {
-            if (distanceSquared2D(player.getLocation(), location) <= 4) {
-                playerLocationsVisited
-                        .computeIfAbsent(player, k -> new HashSet<>())
-                        .add(location);
-
-                if (!playerStartLocation.containsKey(player)) {
-                    playerStartLocation.put(player, player.getLocation());
-                }
-            }
+        if (!playerLocation.getWorld().equals(spawn.getWorld())) {
+            return;
         }
 
-        if (playerLocationsVisited.containsKey(player) &&
-                playerLocationsVisited.get(player).containsAll(requiredLocations) &&
-                distanceSquared2D(player.getLocation(), playerStartLocation.get(player)) <= 4) {
+        double distance = playerLocation.distance(spawn);
 
-            playerLocationsVisited.remove(player);
+        if (distance >= 7 && distance <= 11) {
 
-            int rounds = playerRounds.getOrDefault(player, 0);
-            playerRounds.put(player, rounds + 1);
-            player.sendMessage("§7Umrundet: §d" + (rounds + 1) + " §7mal");
-            player.playSound(player.getLocation(), Sound.LEVEL_UP, 0.5f, 0.5f);
+            int roundCount = user.persistentData().get(KEY_ROUND_COUNT, PersistentDataTypes.INTEGER, ()-> 0);
 
-            // Reset timer
-            if (playerResetTasks.containsKey(player)) {
-                playerResetTasks.get(player).cancel();
-            }
+            roundCount++;
 
-            BukkitRunnable resetTask = new BukkitRunnable() {
-                @Override
-                public void run() {
-                    playerRounds.remove(player);
-                    playerStartLocation.remove(player);
-                    player.sendMessage("§7Deine Rundenzählung wurde zurückgesetzt!");
-                    player.playSound(player.getLocation(), Sound.WOOD_CLICK, 1.0f, 1.5f);
-                }
-            };
+            user.persistentData().set(KEY_ROUND_COUNT, PersistentDataTypes.INTEGER, roundCount);
 
-            resetTask.runTaskLater(plugin, 600);  // 30 seconds (600 ticks)
-            playerResetTasks.put(player, resetTask);
+            player.sendMessage("Umrundet: " + roundCount + " mal");
         }
     }
-
-    private double distanceSquared2D(Location loc1, Location loc2) {
-        double dx = loc1.getX() - loc2.getX();
-        double dz = loc1.getZ() - loc2.getZ();
-        return dx * dx + dz * dz;
-    }
-
-    @EventHandler
-    public void onPlayerQuit(org.bukkit.event.player.PlayerQuitEvent event) {
-        Player player = event.getPlayer();
-
-        playerLocationsVisited.remove(player);
-        playerStartLocation.remove(player);
-        playerRounds.remove(player);
-        if (playerResetTasks.containsKey(player)) {
-            playerResetTasks.get(player).cancel();
-            playerResetTasks.remove(player);
-        }
-    }
-
 }
