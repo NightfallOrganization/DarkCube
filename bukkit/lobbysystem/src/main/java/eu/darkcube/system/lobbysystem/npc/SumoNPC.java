ded8adb31fb7b0d0f9f9fd80c8aeae4d80f230aa
Darkness <darkness@darkcube.eu>
2024-04-10 01:46:31 +0200

    Update

--- bukkit/lobbysystem/src/main/java/eu/darkcube/system/lobbysystem/npc/SumoNPC.java
+++ bukkit/lobbysystem/src/main/java/eu/darkcube/system/lobbysystem/npc/SumoNPC.java
@@ -12,7 +12,7 @@ import eu.darkcube.system.lobbysystem.Lobby;
 public class SumoNPC {
 
     public static NPCManagement.NPC create() {
-        NPCManagement.NPC npc = Lobby.getInstance().npcManagement().builder().profileHelper().name("§fSumo &8❂").skin(new SumoSkin()).builder().build();
+        NPCManagement.NPC npc = Lobby.getInstance().npcManagement().builder().profileHelper().name("§fSumo §8❂").skin(new SumoSkin()).builder().build();
         npc.location(Lobby.getInstance().getDataManager().getSumoNPCLocation());
 
         new NPCKnockbackThread(npc).runTaskTimer(Lobby.getInstance(), 5, 5);
@@ -22,7 +22,7 @@ public class SumoNPC {
     public static class SumoSkin extends NPCManagement.Skin {
 
         public SumoSkin() {
-            super("ewogICJ0aW1lc3RhbXAiIDogMTU5MTYyOTc4Mzc2MSwKICAicHJvZmlsZUlkIiA6ICJiMGM5MjQ2ZTcyMDE0ZDI2YTc3NWFmNDIyMTZkOTUwMSIsCiAgInByb2ZpbGVOYW1lIiA6ICJFbGl0ZURhcmtuZXNzIiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzZkMTg3MzkxZmM2ZmNjYzhhMDdmMmI2NGJiZGUyODgwN2NjZTZjOGZiNzA0MDZiY2U2ZTcxNDg2MDc0YzdjOGMiCiAgICB9CiAgfQp9", "Bgywd/32NsH1kHtHRSz7pxeXTfaOPjFOzLjAFkV+Vjdq4gqCuoEMhkLo8wVXYk8tmUkz53+6VcQjoQhheJ0kjBXWVbLdGO83apAbjveZ4hOezDatW9Vv8YbD8+9vjEFOs8HuPWB1b6630xZKqcNwQqE+Q4q3hJBRxtlnV/qw8SjsJDKaTA8gLtJ/xl2fa5WNprjIvfULr7RUrJCSfB+a9BZKKcESnQ6ta7id+sGw/j0v/0eBBoq975SW0MSdDTuknn1P64qZTzJfEuIv1etTj2F+Wl6JVrEyamtPFKSA+rltbh64Foz4ebIV0JxIkpjCMO3puwsB2P5NqzvbRLjS7Jr0sEgORGpZD4Gck02Q2+cIQ9wuPkiKGn9sO5fGY97gXB4p/zfVOqty56lUclp7nnuw962bTUD8Mkl1jJQWoKlWOjLNVC9FY9XaTMyaReMH4Xgu6FVMPdEzyrGKndJPD6mkwzWCC+QQaeAK6bgaOMewrdct6NLi0NJch/lQSJ1F9mhZffV2dJrIFl4ul7ngxgw2JPxKNr8vAEFI00HlaLBEm38Zy0A4sj4H/7iYTU+/0i7QQio+/AKJWGcOQw8ZHvfCG1L8GvxYUTp15N/Nn9UNyiBeAvrKEnz6vVTHQ16qJgS0N8wBvYipKIxY6JF483VS+PMsaxtpAi5uhKi4X9s=");
+            super("ewogICJ0aW1lc3RhbXAiIDogMTcxMjcwNjIzNzYxNywKICAicHJvZmlsZUlkIiA6ICI4ZjE5NjJmYzE4NzY0MDU3ODYxMmIxMzNjODE4YmY5OSIsCiAgInByb2ZpbGVOYW1lIiA6ICJOaW9uXzkiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNmJkY2QzYjVjMjUzMzc4NDFhMGU4OGM0OTQzNWYyYzg0NjhiN2I2M2ZkNmZiNWQzYmRlYzU1YzMyNmM4MzI5ZiIKICAgIH0KICB9Cn0=", "yiV4JkpsMLbhpzTyjUlTVDlfxoZiXJUzQZDc3UMOUu27gEWTEy4wuTngZttPlOGYwTs8tKcnhF8fEvuUPsWLsdJ/wtDi6YOQQFzVKgb0Jq6E3thKIu34txpzyfMkMojywp2sZWYk8KgY8HmPX9CuHOHZgn8xuuyi4UsgUpNiFBlr8F2Ne3hHpJt0TtL7oQ3skLE6csFqc3NbpRzBcvbOu9Xr7y+DDx8dVE4bzj0C0nZq+FTeH1V/+ZVkPdi5y8dqr1tBX/g26wswieo1jut37SA9QyNLmCcOe9pWlTA3bYN7QxNazJqlgFRY302SW7JtvtPWnwiK4+qCIgrWvIqLdqv1RFWUrmlZ/QHe3g2jNFq3MF6H4JngKw/x7Iy0hEJ/WsDj6+U+O6FEqiafbl7c53LHKlxt9LO303DT1O3tijtc7K0U3PAD/ZrHkAYTKiwa/wbQjTXPRlT6C02wiwQj/lThnXM+TwdzUqwlGeTmlcw0w4gyvYNTrs7HUaUKC6DHtXo9yrgTj076jSu3zfUSunuJtd2D6NCSkMwxHy3DR4QkttC/LwMQE+m/tyP3KNE8PKNfkulMOEWo1CXJKgo7mCJy44WJZlqnOMDlhgE/0tB3UyndYZd1zjcsqpvhjgqjgyv8fV35TDmaKqq/fPq5/UnHD3qVn/ezSovTdvZKclQ=");
         }
 
     }
