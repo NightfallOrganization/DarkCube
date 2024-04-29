3ed3c4b1aec75f8e6ab931e050b6fe7becebc19b
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2024-04-29 12:30:26 +0200

    use darkcube plugin

--- darkcubesystem/velocity/src/main/java/eu/darkcube/system/velocity/DarkCubeVelocity.java
+++ darkcubesystem/velocity/src/main/java/eu/darkcube/system/velocity/DarkCubeVelocity.java
@@ -10,7 +10,6 @@ package eu.darkcube.system.velocity;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.LoginEvent;
-import com.velocitypowered.api.event.connection.PreLoginEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
