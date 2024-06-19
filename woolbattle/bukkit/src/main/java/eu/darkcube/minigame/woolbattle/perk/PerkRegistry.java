5e755897625fbd373105be9cb0f63a529a678822
Darkness <darkness@darkcube.eu>
2024-06-19 16:20:46 +0200

    Fehlerbehebung beim Piercing und Knockback PerkItem

--- woolbattle/bukkit/src/main/java/eu/darkcube/minigame/woolbattle/perk/PerkRegistry.java
+++ woolbattle/bukkit/src/main/java/eu/darkcube/minigame/woolbattle/perk/PerkRegistry.java
@@ -60,7 +60,7 @@ public class PerkRegistry {
         register(new HookArrowPerk(woolbattle));
         register(new DrawArrowPerk(woolbattle));
         register(new PiercingArrowPerk(woolbattle));
-        register(new KnockbackArrowPerk(woolbattle));
+//        register(new KnockbackArrowPerk(woolbattle));
         register(new FreezeArrowPerk(woolbattle));
         register(new BerserkerPerk(woolbattle));
         register(new StomperPerk(woolbattle));
