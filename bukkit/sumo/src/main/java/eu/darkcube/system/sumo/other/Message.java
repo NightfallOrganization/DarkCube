906ca2626030e378d4a9014bec147797b90ecc5f
Darkness <darkness@darkcube.eu>
2024-06-19 15:17:50 +0200

    New Knockback Arrow Perk in WoolBattle

---
+++ bukkit/sumo/src/main/java/eu/darkcube/system/sumo/other/Message.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2024. [DarkCube]
+ * All rights reserved.
+ * You may not use or redistribute this software or any associated files without permission.
+ * The above copyright notice shall be included in all copies of this software.
+ */
+
+package eu.darkcube.system.sumo.other;
+
+import eu.darkcube.system.BaseMessage;
+
+public enum Message implements BaseMessage {
+    MINIMUM_REQUIREMENT,
+    DONT_HAVE_PERMISSION;
+
+
+    public static final String PREFIX_ITEM = "ITEM_";
+    public static final String PREFIX_LORE = "LORE_";
+    public static final String KEY_PREFIX = "LOBBY_";
+
+    private final String key;
+
+    Message() {
+        key = name();
+    }
+
+    @Override public String getPrefixModifier() {
+        return KEY_PREFIX;
+    }
+
+    @Override public String key() {
+        return key;
+    }
+
+}
