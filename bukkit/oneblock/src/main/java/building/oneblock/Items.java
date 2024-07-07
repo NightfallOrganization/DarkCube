1c90af196f6babf084a36b890b074055dcd01d06
Darkness <darkness@darkcube.eu>
2024-07-07 23:03:13 +0200

    Item Enum Zeug

--- bukkit/oneblock/src/main/java/building/oneblock/Items.java
+++ bukkit/oneblock/src/main/java/building/oneblock/Items.java
@@ -8,27 +8,39 @@
 package building.oneblock;
 
 import static eu.darkcube.system.server.item.ItemBuilder.item;
-import static org.bukkit.Material.IRON_SWORD;
-import static org.bukkit.Material.STONE_SWORD;
+import static org.bukkit.Material.*;
 
 import building.oneblock.util.Message;
 import eu.darkcube.system.libs.net.kyori.adventure.text.Component;
 import eu.darkcube.system.server.item.ItemBuilder;
+import eu.darkcube.system.server.item.meta.LeatherArmorBuilderMeta;
 import eu.darkcube.system.userapi.User;
 import eu.darkcube.system.userapi.UserAPI;
+import org.bukkit.Color;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
 public enum Items {
     CUSTOM_STONE_SWORD_1(item(STONE_SWORD)),
     CUSTOM_STONE_SWORD_2(item(STONE_SWORD)),
-    CUSTOM_IRON_SWORD(item(IRON_SWORD))
-    ;
+    DRAGON_CHESTPLATE(item(LEATHER_CHESTPLATE).attributeModifier(Attribute.GENERIC_MOVEMENT_SPEED, new AttributeModifier("dragon_boost", 1, AttributeModifier.Operation.ADD_NUMBER)).meta(new LeatherArmorBuilderMeta(new eu.darkcube.system.util.Color(Color.PURPLE.asRGB())))),
+    CUSTOM_IRON_SWORD(item(IRON_SWORD));
 
     private final ItemBuilder builder;
 
     Items(ItemBuilder builder) {
         this.builder = builder;
+    }
+
+    @EventHandler
+    public void handle(InventoryClickEvent event) {
+        ItemStack itemStack = event.getCurrentItem();
+        ItemBuilder builder = ItemBuilder.item(itemStack);
+
     }
 
     public ItemStack getItem(Player player) {
