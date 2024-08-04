8c3588ff5cfc01bb991ae8be1fbb340cb22939ce
Gogolinolett <50708111+Gogolinolett@users.noreply.github.com>
2024-08-04 09:01:11 +0200

    Mob wip

--- bukkit/skyland/src/main/java/eu/darkcube/system/skyland/mobs/AbstractHostileMob.java
+++ bukkit/skyland/src/main/java/eu/darkcube/system/skyland/mobs/AbstractHostileMob.java
@@ -7,6 +7,7 @@
 
 package eu.darkcube.system.skyland.mobs;
 
+import eu.darkcube.system.skyland.Skyland;
 import eu.darkcube.system.skyland.equipment.Material;
 import eu.darkcube.system.skyland.equipment.PlayerStats;
 import eu.darkcube.system.skyland.equipment.Rarity;
@@ -19,9 +20,13 @@ import java.util.List;
 
 public abstract class AbstractHostileMob extends AbstractCustomMob{
 
+    //location from which aggro range etc are calculated
+    NamespacedKey originLocationKey = new NamespacedKey(Skyland.getInstance(), "originLocation");
+    NamespacedKey aggroRangeKey = new NamespacedKey(Skyland.getInstance(), "aggroRange");
+    NamespacedKey tetherRangeKey = new NamespacedKey(Skyland.getInstance(), "tetherRange");
 
-
-    public AbstractHostileMob(Mob mob, int dmg, PlayerStats[] stats, int speed, boolean isAware, String name, Rarity rarity, MonsterAbility[] monsterAbilities) {
+    public AbstractHostileMob(Mob mob, int dmg, PlayerStats[] stats, int speed, boolean isAware, String name, Rarity rarity, MonsterAbility[] monsterAbilities,
+                              Location origin, int aggroRange, int tetherRange) {//todo finish consturctor with new values
         super(mob, dmg, stats, speed, isAware, name, rarity, monsterAbilities);
     }
 
